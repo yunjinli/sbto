86e5b1f4648a85950f6cebe795ad16fb6c53c71a
Victor Dhedin <victor.dhedin@tum.de>
2025-10-17 16:20:46 +0200

    Update g1_gait example. g1_stand is outdated.

--- examples/g1_gait.py
+++ examples/g1_gait.py
@@ -1,10 +1,7 @@
 import os
 
 from sbto.tasks.unitree_g1.g1_gait import G1_Gait, ConfigG1Gait
-from sbto.mj.solver.cem import CEM
-from sbto.mj.solver.efficient_cem import EfficientCEM, EfficientCEMConfig
-from sbto.utils.plotting import plot_state_control, plot_costs, plot_mean_cov, plot_contact_plan
-from sbto.utils.viewer import render_and_save_trajectory
+from sbto.mj.solver.cem import CEM, CEMConfig, CEMBeta, CEMBetaConfig
 from sbto.utils.exp_manager import run_experiments
 
 def main():
@@ -14,33 +11,22 @@ def main():
         Nthread=112,
         Nknots=15
     )
-    nlp = G1_Gait(cfg_nlp)
-    cfg_solver = EfficientCEMConfig(
+    cfg_solver = CEMBetaConfig(
         N_samples=1024,
-        elite_frac=0.03,
-        alpha_mean=0.9,
+        elite_frac=0.04,
+        alpha_mean=0.7,
         alpha_cov=0.1,
         seed=42,
         quasi_random=True,
-        N_it=200,
+        N_it=100,
+        kappa=15.
     )
-    solver = EfficientCEM(
-        nlp,
-        cfg_solver
-        )
-    init_state = solver.init_state(
-        mean=None,
-        cov=None,
-        sigma_mult=0.4
-    )
-
     run_experiments(
         G1_Gait,
         cfg_nlp,
-        EfficientCEM,
+        CEMBeta,
         cfg_solver,
-        init_state,
-        description="test"
+        description="cem_b"
     )
 
 if __name__ == "__main__":
